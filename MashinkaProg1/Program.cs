b270855c6c08f70b424fa8469af7bf5b8bbf1468
Den4ik <deniel270202@gmail.com>
2021-04-19 20:33:36 +0300

    Added names in Persons.txt

--- MashinkaProg1/Program.cs
+++ MashinkaProg1/Program.cs
@@ -12,9 +12,12 @@ namespace Mashinka
             string NAMES = @"C:\Coding\VScodes\Mashinki\MashinkaProg1\Data\Persons.txt";
             List<Person> persons = new List<Person>();
             string[] names = File.ReadAllLines(NAMES);
-            Random rand_name = new Random();
-            for (int i = 0; i < names.Length; i++)
-                WriteLine($"{names[rand_name.Next(0,names.Length)]}");
+            Array.Sort(names);
+            StreamWriter sw = new StreamWriter(NAMES, false);
+            for(int i = 0; i < names.Length; i++)
+            {
+                sw.WriteLine($"{names[i]}");
+            }
 
             ReadLine();
         }
